c5f2235326a8a0e76a1d9a81a7041c1b2b9a2105
Paal Joachim Romdahl <paaljoachim@hotmail.com>
2016-06-22 20:59:07 +0200

    Create featured-image-test.php

---
+++ featured-image-test.php
@@ -0,0 +1,15 @@
+/* Testing Uses featured image, if no featured image uses first image in the post, and if no post image uses a default selected image. */
+
+function catch_that_image() {
+  global $post, $posts;
+  $first_img = '';
+  ob_start();
+  ob_end_clean();
+  $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+  $first_img = $matches[1][0];
+
+  if(empty($first_img)) { // Defines a default image.
+    $first_img = "/path/to/default.png";
+  }
+  return $first_img;
+}
