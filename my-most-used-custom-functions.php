3fe824c2a3128bd26ec682074c5c5d928de1bc04
Paal Joachim Romdahl <paaljoachim@hotmail.com>
2016-06-22 22:25:42 +0200

    Update my-most-used-custom-functions.php

--- my-most-used-custom-functions.php
+++ my-most-used-custom-functions.php
@@ -12,43 +12,53 @@ License: GPL2
 /**
  * Include all necessary files
  */
-//admin customizations
+//Adding video play list to the dashboard.
 if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/dashboard-widgets.php' ) ) {
 	require_once( 'lib/dashboard-widgets.php' );
 }
 
-if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/custom.php' ) ) {
-	require_once( 'lib/custom.php' );
+//Adjusting images. All posts image thumbnail and wordcount + featured image check.
+if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/images.php' ) ) {
+	require_once( 'lib/images.php' );
 }
 
+// A custom login screen.
 if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/custom-login.php' ) ) {
 	require_once( 'lib/custom-login.php' );
 }
 
+// A better tool bar.
 if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/top-admin-tool-bar.php' ) ) {
 	require_once( 'lib/top-admin-tool-bar.php' );
 }
 
+// Quick edit: Duplicate post
 if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/duplicate-post.php' ) ) {
 	require_once( 'lib/duplicate-post.php' );
 }
 
+// Add TinyMCE Excerpt to post and to page.
 if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/tinymce-excerpt.php' ) ) {
 	require_once( 'lib/tinymce-excerpt.php' );
 }
 
-// Fontawesome is also loaded.
+// A custom avatar with social media icons. Fontawesome is also loaded.
 if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/custom-authorbox.php' ) ) {
 	require_once( 'lib/custom-authorbox.php' );
 }
 
-if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/back-to-top.php' ) ) {
+// Unhide kitchen sink, Add lorum ipson to blank posts, Custom avatar to disucssion setting, Automatic updates, Replace Howdy, custom message admin bottom, change color of post status.
+if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/custom.php' ) ) {
+	require_once( 'lib/custom.php' );
+}
+
+/*if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/back-to-top.php' ) ) {
 	require_once( 'lib/back-to-top.php' );
 }
 
 if ( file_exists( plugin_dir_path( __FILE__ ) . 'lib/test.php' ) ) {
 	require_once( 'lib/test.php' );
-}
+}*/
 
 
 /* Custom avatar in discussion setting. Used for comments. 
